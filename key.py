7b3fd8068f0f0784aa15cae3df9cc167cff18b93
Thiago Schneider <76605923+ThiagoTSchneider@users.noreply.github.com>
2023-08-30 14:09:00 -0300

    DiscordAsisGPT3
    Neste repositório, você encontrará o código que permite a integração das APIs da OpenAI, Discord e Elevenlabs. Essas poderosas APIs combinadas permitirão adicionar funcionalidades avançadas e inteligentes ao seu servidor do Discord. 🚀
    
    Com a OpenAI, você poderá aproveitar ao máximo o GPT-3 para gerar respostas inteligentes e naturais em suas conversas. Já a API do Discord permitirá uma integração perfeita com a plataforma, oferecendo diversas possibilidades de interação com seus usuários.
    
    E, por último, mas não menos importante, a API da Elevenlabs proporcionará uma experiência única por meio do seu sistema de voz doce e agradável. 👩‍💻🎙️
    
    Espero que isso tenha lhe dado uma boa ideia do que você encontrará neste repositório. Se você tiver alguma pergunta específica ou quiser saber mais detalhes sobre algum aspecto do projeto, fique à vontade para perguntar! 😊

---
+++ key.py
@@ -0,0 +1,5 @@
+#-------------------API_KEYS---------------------#
+
+elevenlabs_key = "<INSERT YOUR API KEY HERE>"
+openai_api = "<INSERT YOUR API KEY HERE>"
+discord_token = "<INSERT YOUR TOKEN HERE>"
---
+++ main.py
@@ -0,0 +1,45 @@
+import discord
+import asyncio
+
+from elevenlabs import generate, save
+from elevenlabs import set_api_key
+
+from key import elevenlabs_key
+from key import discord_token
+from gpt import comp
+
+
+set_api_key(elevenlabs_key)
+voice_model = "Asis"
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+async def generate_and_send_audio(channel, generated_response):
+    print("Initializing Audio Generation\n")
+    audio = generate(text=generated_response, voice=voice_model, model='eleven_multilingual_v1')
+    save(audio, "Asis.mp3")
+    await channel.send(file=discord.File("Asis.mp3"))
+    print("Audio Generated, Asis is Free for New Requests\n")
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="Ei Asis"))
+
+@client.event
+async def on_message(message):
+    if message.content.lower().startswith('ei asis'):
+        print('User Requested GPT-3 Auto Completion')
+        user_input = message.content.lower().split('ei asis',)[1]
+        if user_input:
+            generated_response = comp(user_input)
+            await message.channel.send(generated_response)
+            print("GPT-3 Auto Completion Done\n")
+
+            asyncio.create_task(generate_and_send_audio(message.channel, generated_response))
+        else:
+            await message.channel.send("Konichiwa <@{user}> como posso ajudar?".format(user=message.author.id))
+
+client.run(discord_token)
